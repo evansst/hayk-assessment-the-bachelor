42db283ecc5d597f024fd47e1c84b9118cfefaef
Sam Evans <evansst@icloud.com>
2020-06-23 12:58:49 -0600

    Update bachelor.rb

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,40 +1,41 @@
 require 'pry'
 
-
 def get_first_name_of_season_winner(data, season)
-  season = data["#{season}"].find {|x| x["status"] == "Winner"}["name"].split(' ').first
+  data[season.to_s]
+    .find { |x| x['status'] == 'Winner' }['name']
+    .split(' ').first
 end
 
 def get_contestant_name(data, occupation)
-  result = nil 
-  data.each do |season, contestant|
-    result = contestant.find{|x| x["occupation"] == occupation}
-    return result["name"] if result
+  result = nil
+  data.each do |_season, contestant|
+    result = contestant.find { |x| x['occupation'] == occupation }
+    return result['name'] if result
   end
 end
 
 def count_contestants_by_hometown(data, hometown)
   count = 0
-  data.each do |season, contestant|
-    count = count + contestant.count {|x| x["hometown"] == hometown}
+  data.each do |_season, contestant|
+    count += contestant.count { |x| x['hometown'] == hometown }
   end
   count
 end
 
 def get_occupation(data, hometown)
   result = nil
-  data.each do |season, contestant|
-    result =  contestant.find {|x| x["hometown"] == hometown}
-    return result["occupation"] if result
+  data.each do |_season, contestant|
+    result = contestant.find { |x| x['hometown'] == hometown }
+    return result['occupation'] if result
   end
 end
 
 def get_average_age_for_season(data, season)
   total_age = 0
-  season = data["#{season}"]
+  season = data[season.to_s]
   season.each do |contestant|
-    #binding.pry
-    total_age = total_age + contestant["age"].to_f
+    # binding.pry
+    total_age += contestant['age'].to_f
   end
-  (total_age/season.size).round
+  (total_age / season.size).round
 end
