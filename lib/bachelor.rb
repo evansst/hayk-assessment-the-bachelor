b0374b05e99dd58ee2a0ec9cb771bf71872ed2f4
Sam Evans <evansst@icloud.com>
2020-06-23 10:03:31 -0600

    Update bachelor.rb

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,44 @@
+require 'pry'
+
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  season = data["#{season}"]
+  result = season.find {|x| x["status"] == "Winner"}["name"]
+  result.split(' ').first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  result = nil 
+  data.each do |season, contestant|
+    result = contestant.find{|x| x["occupation"] == occupation}
+    if result
+      return result["name"]
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestant|
+    count = count + contestant.count {|x| x["hometown"] == hometown}
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  result = nil
+  data.each do |season, contestant|
+    result =  contestant.find {|x| x["hometown"] == hometown}
+    return result["occupation"] if result
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total_age = 0
+  season = data["#{season}"]
+  season.each do |contestant|
+    #binding.pry
+    total_age = total_age + contestant["age"].to_f
+  end
+  (total_age/season.size).round
 end
